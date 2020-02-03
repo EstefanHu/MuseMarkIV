1359cc72eda9ef6836be6208708c7f3b7f1573fc
Estefan Hu <estefanhu074@gmail.com>
2020-02-03 14:03:18 -0800

    removed fire

--- client/src/components/hub/hub.js
+++ client/src/components/hub/hub.js
@@ -15,3 +15,12 @@ const Hub = props => {
     </article>
   )
 }
+
+const article = {
+  height: '300px',
+  width: '150px',
+  boxShadow: '12px 12px 12px 0 rgba(0, 0, 0, 0.05)',
+  padding: '20px 30px'
+}
+
+export default Hub;
--- client/src/components/layout/nav.js
+++ client/src/components/layout/nav.js
@@ -3,31 +3,12 @@ import React from 'react';
 const Nav = () => {
   return (
     <nav style={ navBar }>
-      <div className="fire">
-        <div className="fire-left">
-          <div className="main-fire"></div>
-          <div className="particle-fire"></div>
-        </div>
-        <div className="fire-main">
-          <div className="main-fire"></div>
-          <div className="particle-fire"></div>
-        </div>
-        <div className="fire-right">
-          <div className="main-fire"></div>
-          <div className="particle-fire"></div>
-        </div>
-        <div className="fire-bottom">
-          <div className="main-fire"></div>
-        </div>
-      </div>
+      
     </nav>
   )
 }
 
 const navBar = {
-  position: 'absolute',
-  bottom: '70px',
-  right: '100px',
 }
 
 export default Nav;
