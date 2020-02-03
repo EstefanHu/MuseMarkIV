34aa109d39568180e72d24d8c2d68586eb5064cf
Estefan Hu <estefanhu074@gmail.com>
2020-02-03 11:46:18 -0800

    removed unused

---
+++ server/server.js
@@ -0,0 +1,25 @@
+'use strict';
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const DB_CONNECTION = 'musedb';
+mongoose.connect(`mongodb://localhost/${DB_CONNECTION}`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
+
+mongoose.connection.once('open', () => {
+    console.log(`connection has been established to ${DB_CONNECTION}`);
+}).on('err', err => {
+    console.log('Connection Error: ' + err);
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
