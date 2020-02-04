3c7a1f8b48ebb5cedee1309abc20975b6fff8396
Estefan Hu <estefanhu074@gmail.com>
2020-02-04 14:25:02 -0800

    Setting up for prototype

--- client/src/components/layout/nav.js
+++ client/src/components/layout/nav.js
@@ -9,6 +9,12 @@ const Nav = () => {
 }
 
 const navBar = {
+  // position: 'fixed',
+  // left: '0',
+  // top: '0',
+  // bottom: '0',
+  // width: '300px',
+  // backgroundColor: 'grey'
 }
 
 export default Nav;
