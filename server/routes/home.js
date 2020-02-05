c894fa12829d79d41bed4a84d98f9384d0fe7aee
Estefan Hu <estefanhu074@gmail.com>
2020-02-04 19:02:30 -0800

    updated server

--- server/routes/home.js
+++ server/routes/home.js
@@ -2,27 +2,28 @@ const router = require('express').Router();
 
 router.route('/').get((req, res) => {
   res.json({"hubs": [
-    {
-      "id": 1,
-      "title": "Write Away!",
-      "img": "writeaway!.jpg"
-    },
-    {
-      "id": 2,
-      "title": "AU",
-      "img": "au.jpg"
-    },
-    {
-      "id": 3,
-      "title": "UW Poetry Club",
-      "img": "uwpoetryclub.jpg"
-    },
-    {
-      "id": 4,
-      "title": "ASUWQSC",
-      "img": "asuwqsc.jpg"
-    }
-  ]});
+      {
+        "id": 1,
+        "title": "Write Away!",
+        "img": "writeaway!.jpg"
+      },
+      {
+        "id": 2,
+        "title": "AU",
+        "img": "au.jpg"
+      },
+      {
+        "id": 3,
+        "title": "UW Poetry Club",
+        "img": "uwpoetryclub.jpg"
+      },
+      {
+        "id": 4,
+        "title": "ASUWQSC",
+        "img": "asuwqsc.jpg"
+      }
+    ]
+  });
 });
 
 module.exports = router;
