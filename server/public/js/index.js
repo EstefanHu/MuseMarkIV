8137d5c582522048285c0fdbbfd0635b21422933
Estefan Hu <estefanhu074@gmail.com>
2020-02-08 19:20:26 -0800

    Set EOD:

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -6,8 +6,9 @@
     fetch('http://localhost:4000/trending')
       .then(res => res.json())
       .then(res => renderTrending(res))
-      .then(carousel)
       .catch(console.error);
+
+    carousel();
   }
 
   function renderTrending(res) {
@@ -17,17 +18,31 @@
   function carousel() {
     const track = document.getElementById('carousel_track');
     const slides = Array.from(track.children);
-    const nextButton = document.getElementById('carousel_button--left');
-    const prevButton = document.getElementById('carousel_button--right');
+    const nextButton = document.getElementById('carousel_button--right');
+    console.log(nextButton)
+    const prevButton = document.getElementById('carousel_button--left');
     const dotsNav = document.getElementById('carousel_nav');
     const dots = Array.from(dotsNav.children);
 
     const slideWidth = slides[0].getBoundingClientRect().width;
 
-    slides[0].style.left = slideWidth * 0 + 'px';
-    slides[1].style.left = slideWidth * 1 + 'px';
-    slides[2].style.left = slideWidth * 2 + 'px';
+    // slides[0].style.left = slideWidth * 0 + 'px';
+    // slides[1].style.left = slideWidth * 1 + 'px';
+    // slides[2].style.left = slideWidth * 2 + 'px';
+
+    const setSlidePosition = (slide, index) => {
+      slide.style.left = slideWidth * index + 'px';
+    }
+    slides.forEach(setSlidePosition);
+
+    nextButton.addEventListener('click', e => {
+      const currSlide = track.querySelector('.current_slide');
+      const nextSlide = currSlide.nextElementSibling;
+    });
+
   }
+
+  
 
   
 })();
